11c88cfd5e8c413395041ef1f09f754d7ac91a0e
calvinpan <calvinpan@google.com>
2021-01-27 21:16:18 +0800

    Use non RCS capabilities cache expiration when result is not found
    Bug: 174196620
    Test: atest
    Change-Id: I1a68e0122cb2ba772e50f45de3b25d26a1bf12b8

--- tests/src/com/android/ims/rcs/uce/eab/EabControllerTest.java
+++ tests/src/com/android/ims/rcs/uce/eab/EabControllerTest.java
@@ -16,7 +16,9 @@
 
 package com.android.ims.rcs.uce.eab;
 
+import static android.telephony.CarrierConfigManager.Ims.KEY_NON_RCS_CAPABILITIES_CACHE_EXPIRATION_SEC_INT;
 import static android.telephony.ims.RcsContactUceCapability.REQUEST_RESULT_FOUND;
+import static android.telephony.ims.RcsContactUceCapability.REQUEST_RESULT_NOT_FOUND;
 import static android.telephony.ims.RcsContactUceCapability.SOURCE_TYPE_NETWORK;
 
 import static com.android.ims.rcs.uce.eab.EabProvider.CONTACT_URI;
@@ -24,6 +26,7 @@ import static com.android.ims.rcs.uce.eab.EabProvider.CONTACT_URI;
 import android.content.ContentValues;
 import android.net.Uri;
 import android.os.Looper;
+import android.os.PersistableBundle;
 import android.telephony.ims.RcsContactPresenceTuple;
 import android.telephony.ims.RcsContactUceCapability;
 import android.test.mock.MockContentResolver;
@@ -44,11 +47,13 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.TimeZone;
 
 @RunWith(AndroidJUnit4.class)
 public class EabControllerTest extends ImsTestBase {
     EabProviderTestable mEabProviderTestable = new EabProviderTestable();
     EabControllerImpl mEabController;
+    PersistableBundle mBundle;
 
     private static final int TEST_SUB_ID = 1;
     private static final String TEST_PHONE_NUMBER = "16661234567";
@@ -72,6 +77,8 @@ public class EabControllerTest extends ImsTestBase {
         insertContactInfoToDB();
         mEabController = new EabControllerImpl(
                 mContext, TEST_SUB_ID, null, Looper.getMainLooper());
+
+        mBundle = mContextFixture.getTestCarrierConfigBundle();
     }
 
     @After
@@ -125,16 +132,69 @@ public class EabControllerTest extends ImsTestBase {
                 mEabController.getCapabilities(contactUriList).get(0).getStatus());
     }
 
-    private RcsContactUceCapability createPresenceCapability(boolean isExpired) {
-        String timeStamp;
+    @Test
+    @SmallTest
+    public void testNonRcsCapability() {
+        // Set non-rcs capabilities expiration to 121 days
+        mBundle.putInt(KEY_NON_RCS_CAPABILITIES_CACHE_EXPIRATION_SEC_INT, 121 * 24 * 60 * 60);
+        // Set timestamp to 120 days age
         GregorianCalendar date = new GregorianCalendar();
+        date.setTimeZone(TimeZone.getTimeZone("UTC"));
+        date.add(Calendar.DATE, -120);
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
+                .format(date.getTime());
+
+        List<RcsContactUceCapability> contactList = new ArrayList<>();
+        contactList.add(createPresenceNonRcsCapability(timestamp));
+
+        mEabController.saveCapabilities(contactList);
+
+        List<Uri> contactUriList = new ArrayList<>();
+        contactUriList.add(TEST_CONTACT_URI);
+
+        // Verify result is not expired
+        Assert.assertEquals(1,
+                mEabController.getCapabilities(contactUriList).size());
+        Assert.assertEquals(EabCapabilityResult.EAB_QUERY_SUCCESSFUL,
+                mEabController.getCapabilities(contactUriList).get(0).getStatus());
+    }
+
+    @Test
+    @SmallTest
+    public void testNonRcsCapabilityExpired() {
+        // Set non-rcs capabilities expiration to 119 days
+        mBundle.putInt(KEY_NON_RCS_CAPABILITIES_CACHE_EXPIRATION_SEC_INT, 119 * 24 * 60 * 60);
+        // Set timestamp to 120 days age
+        GregorianCalendar date = new GregorianCalendar();
+        date.setTimeZone(TimeZone.getTimeZone("UTC"));
+        date.add(Calendar.DATE, -120);
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
+                .format(date.getTime());
+
+        List<RcsContactUceCapability> contactList = new ArrayList<>();
+        contactList.add(createPresenceNonRcsCapability(timestamp));
+        mEabController.saveCapabilities(contactList);
+
+        // Verify result is expired
+        List<Uri> contactUriList = new ArrayList<>();
+        contactUriList.add(TEST_CONTACT_URI);
+        Assert.assertEquals(1,
+                mEabController.getCapabilities(contactUriList).size());
+        Assert.assertEquals(EabCapabilityResult.EAB_CONTACT_EXPIRED_FAILURE,
+                mEabController.getCapabilities(contactUriList).get(0).getStatus());
+    }
+
+    private RcsContactUceCapability createPresenceCapability(boolean isExpired) {
+        String timestamp;
+        GregorianCalendar date = new GregorianCalendar();
+        date.setTimeZone(TimeZone.getTimeZone("UTC"));
         if (isExpired) {
             date.add(Calendar.DATE, -120);
         } else {
             date.add(Calendar.DATE, 120);
         }
 
-        timeStamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
+        timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX")
                 .format(date.getTime());
 
         RcsContactPresenceTuple.ServiceCapabilities.Builder serviceCapabilitiesBuilder =
@@ -146,7 +206,7 @@ public class EabControllerTest extends ImsTestBase {
                         .setServiceDescription(TEST_SERVICE_DESCRIPTION)
                         .setContactUri(TEST_CONTACT_URI)
                         .setServiceCapabilities(serviceCapabilitiesBuilder.build())
-                        .setTimestamp(String.valueOf(timeStamp))
+                        .setTimestamp(timestamp)
                         .build();
 
         RcsContactPresenceTuple tupleWithEmptyServiceCapabilities =
@@ -154,7 +214,7 @@ public class EabControllerTest extends ImsTestBase {
                         TEST_SERVICE_VERSION)
                         .setServiceDescription(TEST_SERVICE_DESCRIPTION)
                         .setContactUri(TEST_CONTACT_URI)
-                        .setTimestamp(String.valueOf(timeStamp))
+                        .setTimestamp(timestamp)
                         .build();
 
         RcsContactUceCapability.PresenceBuilder builder =
@@ -162,6 +222,25 @@ public class EabControllerTest extends ImsTestBase {
                         TEST_CONTACT_URI, SOURCE_TYPE_NETWORK, REQUEST_RESULT_FOUND);
         builder.addCapabilityTuple(tupleWithServiceCapabilities);
         builder.addCapabilityTuple(tupleWithEmptyServiceCapabilities);
+        return builder.build();
+    }
+
+    private RcsContactUceCapability createPresenceNonRcsCapability(String timestamp) {
+        RcsContactPresenceTuple.ServiceCapabilities.Builder serviceCapabilitiesBuilder =
+                new RcsContactPresenceTuple.ServiceCapabilities.Builder(false, false);
+        RcsContactPresenceTuple tupleWithServiceCapabilities =
+                new RcsContactPresenceTuple.Builder(TEST_SERVICE_STATUS, TEST_SERVICE_SERVICE_ID,
+                        TEST_SERVICE_VERSION)
+                        .setServiceDescription(TEST_SERVICE_DESCRIPTION)
+                        .setContactUri(TEST_CONTACT_URI)
+                        .setServiceCapabilities(serviceCapabilitiesBuilder.build())
+                        .setTimestamp(timestamp)
+                        .build();
+
+        RcsContactUceCapability.PresenceBuilder builder =
+                new RcsContactUceCapability.PresenceBuilder(
+                        TEST_CONTACT_URI, SOURCE_TYPE_NETWORK, REQUEST_RESULT_NOT_FOUND);
+        builder.addCapabilityTuple(tupleWithServiceCapabilities);
         return builder.build();
     }
 
