762e3c8028249e154ea61c0317149c0cd7de3fa1
Calvin Pan <calvinpan@google.com>
2021-03-05 15:32:01 +0800

    Fix flaky unit test
    Check handler whether has any message ongoing to prevent timing issue
    
    Bug: 181892680
    Test: atest
    Change-Id: Ia8a93f3b9d0374674e3e012ffc294e53e43f6475

--- src/java/com/android/ims/rcs/uce/eab/EabControllerImpl.java
+++ src/java/com/android/ims/rcs/uce/eab/EabControllerImpl.java
@@ -94,7 +94,7 @@ public class EabControllerImpl implements EabController {
                 this,
                 new EabContactSyncController(),
                 mUceControllerCallback,
-                looper);
+                mHandler);
     }
 
     @Override
