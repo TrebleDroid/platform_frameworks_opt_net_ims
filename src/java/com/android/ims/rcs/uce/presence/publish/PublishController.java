d92dddbbf088de45847f0109782b12bfabdf9de0
Hyunho <hhshin@google.com>
2021-12-15 04:03:41 +0000

    When the IMS unregistered, reset the PUBLISH throttle timer
    Use IMS de-registration as a signal to clear PUBLISH throttle times
    
    Bug: b/205860627
    Test: atest PublishControllerImplTest DeviceCapabilityListenerTest
    Change-Id: I61746586239a28bbfb4952f1792f7a283d728d9b
    Merged-In: I61746586239a28bbfb4952f1792f7a283d728d9b

--- src/java/com/android/ims/rcs/uce/presence/publish/PublishController.java
+++ src/java/com/android/ims/rcs/uce/presence/publish/PublishController.java
@@ -84,6 +84,9 @@ public interface PublishController extends ControllerBase {
     /** The Carrier Config for the subscription has Changed **/
     int PUBLISH_TRIGGER_CARRIER_CONFIG_CHANGED = 16;
 
+    /** MMTEL and RCS are unregistered. **/
+    int PUBLISH_TRIGGER_MMTEL_RCS_UNREGISTERED = 17;
+
     @IntDef(value = {
             PUBLISH_TRIGGER_SERVICE,
             PUBLISH_TRIGGER_RETRY,
@@ -100,7 +103,8 @@ public interface PublishController extends ControllerBase {
             PUBLISH_TRIGGER_RCS_URI_CHANGE,
             PUBLISH_TRIGGER_PROVISIONING_CHANGE,
             PUBLISH_TRIGGER_OVERRIDE_CAPS,
-            PUBLISH_TRIGGER_CARRIER_CONFIG_CHANGED
+            PUBLISH_TRIGGER_CARRIER_CONFIG_CHANGED,
+            PUBLISH_TRIGGER_MMTEL_RCS_UNREGISTERED
     }, prefix="PUBLISH_TRIGGER_")
     @Retention(RetentionPolicy.SOURCE)
     @interface PublishTriggerType {}
@@ -154,6 +158,11 @@ public interface PublishController extends ControllerBase {
          * Sent the publish request to ImsService.
          */
         void notifyPendingPublishRequest();
+
+        /**
+         * Update the Ims unregistered. This api will be called if the IMS unregistered.
+         */
+        void updateImsUnregistered();
     }
 
     /**
